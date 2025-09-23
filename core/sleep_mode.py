b3a629aa13efce5cce905a630b40837b98bfada2
acamadeo <anthony.c.amadeo@gmail.com>
2025-09-23 16:40:31 -0700

    Initial commands

---
+++ core/sleep_mode.py
@@ -0,0 +1,33 @@
+from talon import Context, Module, actions
+
+mod = Module()
+
+wake_ctx = Context()
+
+
+@mod.action_class
+class Actions:
+    def maybe_hide_history():
+        """Hides history if mode wants it"""
+
+    def sleep_all():
+        """Sleeps talon and hides everything"""
+        actions.user.switcher_hide_running()
+        actions.user.maybe_hide_history()
+        actions.user.homophones_hide()
+        actions.user.help_hide()
+        actions.user.mouse_sleep()
+        actions.speech.disable()
+
+    def wake_all():
+        """Wakes talon and shows everything"""
+        actions.user.cancel_in_flight_phrase()
+        actions.user.mouse_wake()
+        actions.user.talon_mode()
+        actions.mode.enable("user.sketch")
+
+
+@wake_ctx.action_class("user")
+class WakeUserActions:
+    def maybe_hide_history():
+        pass
